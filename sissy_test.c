e24798d2137bf1797c55622c7d6888df4fc00dc1
SissySarah <111539427+SissySarah@users.noreply.github.com>
2022-08-18 23:53:42 +0200

    Add files via upload

---
+++ sissy_test.c
@@ -0,0 +1,798 @@
+#include <stdio.h>
+#define BETWEEN(value, min, max) (value < max && value > min)
+
+void result()
+{
+printf("**************\n");
+printf("YOUR RESULT IS\n");
+printf("**************\n");
+}
+
+int main()
+{
+int score, yesorno;
+
+score = 0;
+yesorno = 0;
+
+printf("welcome to the ultimate test \nif you done it type 1 of not type 0\n");
+printf("\nHave you ever fingered your ass ?\n");
+scanf("%d", &yesorno);
+
+if(yesorno == 1)
+{
+    score = score + 1;
+    printf("\nhave you ever used an anal plug ?\n");
+    scanf("%d", &yesorno);
+} 
+else
+{
+    printf("\nhave you ever used an anal plug ?\n");
+    scanf("%d", &yesorno);
+}  
+if(yesorno == 1)
+{
+    score = score + 1;
+    printf("\nhave you ever used a plug in public ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever used a plug in public ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 1;
+    printf("\nhave you ever slept with a plug ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever slept with a plug ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 2;
+    printf("\nhave you ever been straponed ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever been straponed ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 2;
+    printf("\nhave you ever used an household item for anal ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever used an household item for anal ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 1;
+    printf("\nhave you ever been fucked by a man ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever been fucked by a man ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 2;
+    printf("\nhave you ever inserted food in your ass ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever inserted food in your ass ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 1;
+    printf("\nhave you ever fucked a dildo ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever fucked a dildo ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 1;
+    printf("\nhave you ever been gangbanged ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever been gangbanged ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 2;
+    printf("\nhave you ever been creampied ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever been creampied ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 2;
+    printf("\nhave you ever cummed with his dick in your ass ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever cummed with his dick in your ass ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 2;
+    printf("\nhave you ever been spitthroated ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever been spitthroated ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 2;
+    printf("\nhave you ever been fucked in public ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever been fucked in public ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 2;
+    printf("\nhave you ever ate your own cum ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever ate your own cum ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 1;
+    printf("\nhave you ever cummed on your face ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever cummed on your face ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 1;
+    printf("\nhave you ever been in public with cum on your face ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever been in public with cum on your face ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 1;
+    printf("\nhave you ever ate your cum on some food ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever ate your cum on some food ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 1;
+    printf("\nhave you ever ate your cum from a creampie ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever ate your cum from a creampie ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 1;
+    printf("\nhave you ever ate a creampie from another guy ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever ate a creampie from another guy ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 2;
+    printf("\nhave you ever recieved a facial ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever recieved a facial ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 1;
+    printf("\nhave you ever swallowed cum ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever swallowed cum ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 1;
+    printf("\nhave you ever licked up cum that was in your ass ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever licked up cum that was in your ass ?\n");
+    scanf("%d", &yesorno);
+}if(yesorno == 1)
+{
+    score = score + 2;
+    printf("\nhave you ever sucked a dildo ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever sucked a dildo ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 1;
+    printf("\nhave you ever deepthroated 100+ times on a dildo during one day ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever deepthroated 100+ times on a dildo during one day ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 1;
+    printf("\nhave you ever deepthroated a dildo in public ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever deepthroated a dildo in public ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 2;
+    printf("\nhave you ever deepthroated on food ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever deepthroated on food ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 1;
+    printf("\nhave you ever spittthroated dildos ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever spittthroated dildos ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 1;
+    printf("\nhave you ever kissed a guy ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever kissed a guy ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 1;
+    printf("\nhave you ever sucked a strapon ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever sucked a strapon ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 1;
+    printf("\nhave you ever sucked a dick ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever sucked a dick ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 1;
+    printf("\nhave you ever been throatfucked ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever been throatfucked ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 2;
+    printf("\nhave you ever puked while sucking ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever puked while sucking ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 2;
+    printf("\nhave you ever worn a thong and bra ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever worn a thong and bra ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 1;
+    printf("\nhave you ever worn stockings ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever worn stockings ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 1;
+    printf("\nhave you ever worn slim female jeans ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever worn slim female jeans ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 2;
+    printf("\nhave you ever worn yoga pants or leggins ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever worn yoga pants or leggins ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 1;
+    printf("\nhave you ever worn heels ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever worn heels ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 1;
+    printf("\nhave you ever worn skirt or dress ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever worn skirt or dress ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 1;
+    printf("\nhave you ever worn a lingarie for a whole day ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever worn a lingarie for a whole day ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 2;
+    printf("\nhave you ever worn makeup ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever worn makeup ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 1;
+    printf("\nhave you ever been fully dressed like a girl ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever been fully dressed like a girl ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 1;
+    printf("\nhave you ever worn panties and bra in public ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever worn panties and bra in public ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 1;
+    printf("\nhave you ever worn makeup in public ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever worn makeup in public ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 2;
+    printf("\nhave you ever tryed girl cloths in a stores changing room ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever tryed girl cloths in a stores changing room ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 1;
+    printf("\nhave you ever gone out dressed as a girl ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever gone out dressed as a girl ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 2;
+    printf("\nhave you ever slept with lingarie ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever slept with lingarie ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 1;
+    printf("\nhave you ever worn female jewelery in public ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever worn female jewelery in public ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 2;
+    printf("\nhave you ever bought your own panties and bra ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever bought your own panties and bra ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 1;
+    printf("\nhave you ever thrown out your male underwear ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever thrown out your male underwear ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 2;
+    printf("\nhave you ever bought girly cloths for you (exept a lingarie) ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever bought girly cloths for you (exept a lingarie) ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 1;
+    printf("\nhave you ever thrown out all male cloths ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever thrown out all male cloths ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 2;
+    printf("\nhave you ever worn nipple clamps in public ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever worn nipple clamps in public ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 1;
+    printf("\nhave you ever ate or drank on all fours ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever ate or drank on all fours ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 1;
+    printf("\nhave you ever tucked in your clit ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever tucked in your clit ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 1;
+    printf("\nhave you ever did chores dressing ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever did chores dressing ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 1;
+    printf("\nhave you ever got spanked bright red ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever got spanked bright red ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 1;
+    printf("\nhave you ever tied yourself ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever tied yourself ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 1;
+    printf("\nhave you ever been tied and dressed as a girl ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever been tied and dressed as a girl ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 2;
+    printf("\nhave you ever been tied by a man ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever been tied by a man ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 2;
+    printf("\nhave you ever been caged ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever been caged ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 1;
+    printf("\nhave you ever been caged for 1+ month in a row ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever been caged for 1+ moth in a row ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 2;
+    printf("\nhave you ever been tied by a female ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever been tied by a female ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 1;
+    printf("\nhave you ever served as a sissy for a group ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever served as a sissy for a group ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 2;
+    printf("\nhave you ever made sissy pics/vids for yourself ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever made sissy pics/vids for yourself ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 1;
+    printf("\nhave you ever send sissy pics/vids to your gf/bf ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever send sissy pics/vids to your gf/bf ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 2;
+    printf("\nhave you ever send sissy pics/vids to someone you know irl ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever send sissy pics/vids to someone you know irl ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 3;
+    printf("\nhave you ever posted pics/vids online and got seen by 50+ people ?\n");
+    scanf("%d", &yesorno);
+}
+else
+{
+    printf("\nhave you ever posted pics/vids online and got seen by 50+ people ?\n");
+    scanf("%d", &yesorno);
+}
+if(yesorno == 1)
+{
+    score = score + 3;
+}
+if(BETWEEN(score, 1, 20))
+{
+    result();
+    printf("Your a beginner Sissy, keep going depper");
+}
+if(BETWEEN(score, 21, 40))
+{
+    result();
+    printf("your a naughty girl, your ready for the real thing");
+}
+if(BETWEEN(score, 41, 60))
+{
+    result();
+    printf("your almost their sissy keep going deeper");
+}
+if(BETWEEN(score, 61, 75))
+{
+    result();
+    printf("wow your a true sissy slut");
+}
+if(BETWEEN(score, 76, 90))
+{
+    result();
+    printf("wow your an amazing cum hungry cock slut, good girl");
+}
+if(BETWEEN(score, 91, 100))
+{
+    result();
+    printf("your the perfect slut no one can teach you anymore");
+}
+
+return 0;
+} 
+
+
+
+
